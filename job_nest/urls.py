53063d3806e84c7a60ed02b7f8d1f5838e40dcac
weslife911 <mfonfubikoma@gmail.com>
2025-08-28 02:12:31 +0100

    added settings

--- job_nest/urls.py
+++ job_nest/urls.py
@@ -16,16 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("users.urls")),
     path("", include("jobs.urls"))
-<<<<<<< Updated upstream
-]
-=======
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #
->>>>>>> Stashed changes
